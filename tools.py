786701723479659842cfbe71c3f7a78188eaadc8
chocojoX <joseph.budin@polytechnique.edu>
2017-11-12 13:12:10 +0100

    Added read_image

--- tools.py
+++ tools.py
@@ -1,5 +1,7 @@
 import numpy as np
 import os
+import matplotlib.pyplot as plt
+import re
 
 
 def SVD(X):
@@ -23,11 +25,37 @@ def get_all_conditions(individual):
     files = [f for f in files if '.pgm' in f]
     return files
 
+def read_pgm(filename, byteorder='>'):
+    """Return image data from a raw PGM file as numpy array.
+    Format specification: http://netpbm.sourceforge.net/doc/pgm.html
+    """
+    with open(filename, 'rb') as f:
+        buffer = f.read()
+    try:
+        header, width, height, maxval = re.search(
+            b"(^P5\s(?:\s*#.*[\r\n])*"
+            b"(\d+)\s(?:\s*#.*[\r\n])*"
+            b"(\d+)\s(?:\s*#.*[\r\n])*"
+            b"(\d+)\s(?:\s*#.*[\r\n]\s)*)", buffer).groups()
+    except AttributeError:
+        raise ValueError("Not a raw PGM file: '%s'" % filename)
+    return np.frombuffer(buffer,
+                            dtype='u1' if int(maxval) < 256 else byteorder+'u2',
+                            count=int(width)*int(height),
+                            offset=len(header)
+                            ).reshape((int(height), int(width)))
+
 
 def load_image(individual,condition):
     folder_path = "data/YaleB-Dataset/images"
     individual_path = folder_path + "/yaleB0" + str(individual)
     files = get_all_conditions(individual)
+
+    # assume condition<len(files), "Condition number too high, please choose another one (smaller than %i)" %(len(files))
+    file_name = individual_path + "/" + files[condition]
+    image = read_pgm(file_name)
+    plt.imshow(image, plt.cm.gray)
+    plt.show()
 
 
 def threshold_shrinkage(X, tau):
@@ -35,10 +63,6 @@ def threshold_shrinkage(X, tau):
     X[X >= tau] -= tau
     X[X <= -tau] += tau
     return X
-
-
-
-
 
 
 if __name__=="__main__":
@@ -54,4 +78,4 @@ if __name__=="__main__":
     print("inverse_SVD(SVD(X)) = ")
     print(inverse_SVD(U, Sigma, V))
 
-    get_all_conditions(2)
+    load_image(1, 2)
