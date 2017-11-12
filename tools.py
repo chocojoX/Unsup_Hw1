3885a982a9962396b6e40f4c3489c91cb3036fe0
Léonard Hussenot <leonard.hussenot@gmail.com>
2017-11-12 12:59:06 +0100

    blabla

--- tools.py
+++ tools.py
@@ -46,6 +46,32 @@ def threshold_shrinkage(X, tau):
 
 
 
+def get_all_conditions(individual):
+    folder_path = "data/YaleB-Dataset/images"
+    individual_path = folder_path + "/yaleB0" + str(individual)
+
+    files = os.listdir( individual_path )
+    files = [f for f in files if '.pgm' in f]
+    return files
+
+
+def load_image(individual,condition):
+    folder_path = "data/YaleB-Dataset/images"
+    individual_path = folder_path + "/yaleB0" + str(individual)
+    files = get_all_conditions(individual)
+
+
+def threshold_shrinkage(X, tau):
+    X[np.abs(X) <= tau] = 0
+    X[X >= tau] -= tau
+    X[X <= -tau] += tau
+    return X
+
+
+
+
+
+
 if __name__=="__main__":
     """ Use this main function only to debug """
 
